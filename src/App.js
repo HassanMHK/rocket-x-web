431ad772a34f0be2498d4cb2b0c0814d30578c9f
hassan-khaled <hassanmhk7@gmail.com>
2023-09-05 01:18:47 +0300

    added pages:(register,login,home,nopage), added navbar component, init tailwind config, added styles.css to pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/register';
+import Login from './pages/login';
+import Home from './pages/home';
+import NoPage from './pages/nopage';
+import Navbar from './navbar';
 import './App.css';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Rocket X</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/register' element={(<><Navbar /><Register /></>)} />
+          <Route path='/login' element={(<><Navbar /><Login /></>)} />
+          <Route path='/' element={(<><Navbar /><Home /></>)} />
+          <Route path="*" element={(<><Navbar /><NoPage /></>)} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
